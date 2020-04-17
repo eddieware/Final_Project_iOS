abb7e4bc0818db828019571e826c7bca0dc7152a
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-04-17 16:01:55 -0500

    Initial Commit

---
+++ Final_Project_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Final_Project_iOS
+//
+//  Created by EDUARDO MEJIA on 17/04/20.
+//  Copyright © 2020 EDDIEWARE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
